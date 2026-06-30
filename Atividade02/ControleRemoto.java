d3f765d229e34bd250a02ea785ecec04b275be99
Marlon Ferreira <marlon.ferreira.ssp@gmail.com>
2026-06-30 19:45:23 -0300

    Delete Atividade02 directory

--- Atividade02/ControleRemoto.java
+++
@@ -1,8 +0,0 @@
-public interface ControleRemoto {
-    int volumePadrao = 60;
-    void MudarCanal(int canal);
-    void AumentarVolume(int taxa);
-    void DiminuirVolume(int taxa);
-    boolean LigarTV();
-    boolean DesligarTV();
-}
