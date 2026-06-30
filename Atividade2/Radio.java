a8a3abcaca71cd4de7e4bcb9b61b6c8798ba7971
Marlon Ferreira <marlon.ferreira.ssp@gmail.com>
2026-06-30 19:44:57 -0300

    Delete Atividade2 directory

--- Atividade2/Radio.java
+++
@@ -1,7 +0,0 @@
-public interface Radio {
-    void setEmissora(double emissora, String tipoEmissora);
-    double getEmissora();
-    String getTipoEmissora();
-    void setVolumeRadio(double vol);
-    int getVolumeRadio();
-}
